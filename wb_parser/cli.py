980222799454d5c6d38ebfa1c32ca712ebfd6404
Cyberego <cy8erego@yandex.ru>
2021-05-05 11:14:03 +0700

    refactor: small fixes

--- wb_parser/cli.py
+++ wb_parser/cli.py
@@ -4,7 +4,13 @@ import argparse
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("url", help="URL of WB product page")
-    parser.add_argument("-b", "--brief", action="store_true", default=False, help="Not to parse product descriptions")
+    parser.add_argument(
+        "-b",
+        "--brief",
+        action="store_true",
+        default=False,
+        help="Not to parse product descriptions",
+    )
     args = parser.parse_args()
 
     return args
